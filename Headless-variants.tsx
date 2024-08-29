1339fb9541abfd4475951a444c4ecee5586b3aa5
bkateliya <138436188+bkateliya@users.noreply.github.com>
2024-08-29 11:58:30 +0530

    Create Headless-variants

---
+++ Headless-variants.tsx
@@ -0,0 +1,93 @@
+import React from 'react';
+import {
+  Image as JssImage,
+  Link as JssLink,
+  RichText as JssRichText,
+  ImageField,
+  Field,
+  LinkField,
+} from '@sitecore-jss/sitecore-jss-nextjs';
+
+interface Fields {
+  PromoIcon: ImageField;
+  PromoText: Field<string>;
+  PromoLink: LinkField;
+  PromoText2: Field<string>;
+}
+
+type PromoProps = {
+  params: { [key: string]: string };
+  fields: Fields;
+};
+
+const Promo = (props: PromoProps): JSX.Element => (
+  <div className={`component promo ${props.params.styles}`}>
+    <div className="component-content">
+      <span className="is-empty-hint">Promo</span>
+    </div>
+  </div>
+);
+
+export const Default = (props: PromoProps): JSX.Element => {
+  const id = props.params.RenderingIdentifier;
+  const BackgroundTheme =
+    props.params.BackgroundTheme == 'Dark' ? 'bg-dark text-white' : 'bg-light';
+
+  if (props.fields) {
+    return (
+      <div
+        className={`component promo ${props.params.styles} ${BackgroundTheme}`}
+        id={id ? id : undefined}
+      >
+        <div className="component-content">
+          <div className="field-promoicon">
+            <JssImage field={props.fields.PromoIcon} />
+          </div>
+          <div className="promo-text">
+            <div>
+              <div className="field-promotext">
+                <JssRichText field={props.fields.PromoText} />
+              </div>
+            </div>
+            <div className="field-promolink">
+              <JssLink field={props.fields.PromoLink} />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  return <Promo {...props} />;
+};
+
+export const WithText = (props: PromoProps): JSX.Element => {
+  const id = props.params.RenderingIdentifier;
+  const BackgroundTheme = props.params.BackgroundTheme == 'Dark' ? 'bg-dark' : 'bg-light';
+  if (props.fields) {
+    return (
+      <div
+        className={`component promo ${props.params.styles} ${BackgroundTheme}`}
+        id={id ? id : undefined}
+      >
+        <div className="component-content">
+          <div className="field-promoicon">
+            <JssImage field={props.fields.PromoIcon} />
+          </div>
+          <div className="promo-text">
+            <div>
+              <div className="field-promotext">
+                <JssRichText className="promo-text" field={props.fields.PromoText} />
+              </div>
+            </div>
+            <div className="field-promotext">
+              <JssRichText className="promo-text" field={props.fields.PromoText2} />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  return <Promo {...props} />;
+};
